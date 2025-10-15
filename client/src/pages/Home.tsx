c585e9d9b85953630d61fa2934a1e6675770dd9e
Ottobon-INC <pramod@janmasethu.com>
2025-10-15 07:26:00 +0000

    Restored to '10c30f4b596c03872196084d4db5238800049a49'
    Replit-Restored-To: 10c30f4b596c03872196084d4db5238800049a49

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -88,11 +88,11 @@ const Home = () => {
   return (
     <>
       {/* Responsive Video Section */}
-      <section className="w-full py-4 px-2 sm:px-4 lg:pt-6 lg:pb-0 lg:px-0">
-        <div className="relative w-full mx-auto overflow-hidden rounded-2xl md:rounded-3xl lg:rounded-none">
-          <div className="relative w-full lg:h-[85vh]" style={{ paddingBottom: '56.25%' }}>
+      <section className="w-full py-4 px-2 sm:px-4 lg:py-8 lg:px-8">
+        <div className="relative w-full mx-auto overflow-hidden rounded-2xl md:rounded-3xl">
+          <div className="relative w-full" style={{ paddingBottom: '56.25%' }}>
             <video
-              className="absolute top-0 left-0 w-full h-full object-contain lg:object-cover"
+              className="absolute top-0 left-0 w-full h-full object-contain md:object-cover"
               autoPlay
               loop
               muted
